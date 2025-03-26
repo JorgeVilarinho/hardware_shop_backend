fff5b9c6520682d6a09e97f2a0e3e9f212671f13
Jorge Vilariño <jorgevilarino05@gmail.com>
2025-03-26 18:58:10 +0100

    Make server HTTPS

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import http from 'http';
+import https from 'https';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser'
 import compression from 'compression';
 import cors from 'cors';
+import fs from 'fs';
 import { PORT } from './config.js'
 import router from './router/index.js';
 
@@ -11,7 +13,7 @@ const app = express();
 
 app.use(cors({
   credentials: true,
-  origin: [ 'http://byteshop.com', 'http://www.byteshop.com' ]
+  origin: [ 'http://byteshop.com', 'http://www.byteshop.com', 'https://byteshop.com', 'https://www.byteshop.com' ]
 }));
 
 app.use(compression());
@@ -21,8 +23,15 @@ app.use(express.static('public'));
 
 app.use('/', router());
 
-const server = http.createServer(app);
+const options = {
+  key: fs.readFileSync('/etc/nginx/ssl/backend-byteshop.key'),
+  cert: fs.readFileSync('/etc/nginx/ssl/backend-byteshop.crt'),
+}
+
+// const server = http.createServer(app);
+
+const server = https.createServer(options, app);
 
 server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on https://localhost:${PORT}`);
 });
