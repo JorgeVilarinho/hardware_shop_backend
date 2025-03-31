5bdd9fd9e06c530520d3dc1f0647097543831295
Jorge Vilariño <jorgevilarino05@gmail.com>
2025-03-31 19:35:18 +0200

    Change host variable to pool

--- src/db.ts
+++ src/db.ts
@@ -3,7 +3,7 @@ import pg from 'pg';
 export const pool = new pg.Pool({
   user: 'ucha',
   password: 'ucha',
-  host: '192.168.1.120',
+  host: '192.168.56.120',
   port: 5432,
   database: 'hardwareshop'
 });
