e204fc83f7abaf5d56a733f0e659c7e05e729d97
Jorge Vilariño <jorgevilarino05@gmail.com>
2025-05-14 21:45:39 +0200

    Fix 500 error with mappings

--- src/router/products.ts
+++ src/router/products.ts
@@ -6,8 +6,8 @@ export default (router: express.Router) => {
   router.get('/api/products', getProducts);
   router.post('/api/products/product', isAuthenticated, isAdmin, addProduct);
   router.delete('/api/products/product/:id', isAuthenticated, isAdmin, deleteProduct);
+  router.get('/api/products/maxPrice', getMaxProductPrice);
   router.get('/api/products/:id', getProductById);
   router.put('/api/products/:id', isAuthenticated, isAdmin, updateProductById);
   router.get('/api/products/:id/stock', getAvailableStock);
-  router.get('/api/products/maxPrice', getMaxProductPrice);
 }
